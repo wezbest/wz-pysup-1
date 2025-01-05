c9fd0375052de548a2e54b11f0c29ad14f18a654
wezbest <110793713+wezbest@users.noreply.github.com>
2025-01-05 17:38:56 +0000

    🫦

--- WO/ps1/setz.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Setting panty 
-alias SUPABASE_URL="https://gfegdgkjtdayarqebzqr.supabase.co"
-alias SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmZWdkZ2tqdGRheWFycWVienFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYwODUxNTksImV4cCI6MjA1MTY2MTE1OX0.Jgk8B0gY7ruEK4OFRQho1-N_FyEalXiRGrP2T3S8zk"
-
-# Display Panty 
-echo $SUPABASE_URL
-echo $SUPABASE_KEY
--- WO/ps1/src/f1.py
+++ WO/ps1/src/f1.py
@@ -2,15 +2,19 @@
 
 import os
 from supabase import create_client, Client
+from dotenv import load_dotenv
 from rich import print as rprint  # For rprinting
 from rich.pretty import pprint  # For pretty printing
 
+# Custon en
+load_dotenv(dotenv_path="pee")
 
 # Env setup
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("SUB")
+key: str = os.environ.get("SUP")
 supabase: Client = create_client(url, key)
 
+
 def read_table():
-    response = supabase.table("countries").select("*").execute()
+    response = supabase.table("dancers").select("*").execute()
     pprint(response)
