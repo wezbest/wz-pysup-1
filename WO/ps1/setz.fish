5c76ab7497e45961cd8299e39ef2b999df7dea0f
wezbest <110793713+wezbest@users.noreply.github.com>
2025-01-05 16:37:06 +0000

    🫦

---
+++ WO/ps1/setz.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+# Setting panty 
+set -x SUPABASE_URL https://gfegdgkjtdayarqebzqr.supabase.co
+set -x SUPABASE_KEY eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmZWdkZ2tqdGRheWFycWVienFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYwODUxNTksImV4cCI6MjA1MTY2MTE1OX0.Jgk8B0gY7ruEK4OFRQho1-N_FyEalXiRGrP2T3S8zk
+
+# Display Panty 
+echo $SUPABASE_URL
+echo $SUPABASE_KEY
