10f96491acb2dcacf8a02311f38cb468dacfcbaa
wezbest <110793713+wezbest@users.noreply.github.com>
2025-01-05 17:12:44 +0000

    🫦

--- WO/ps1/panty.py
+++ WO/ps1/panty.py
@@ -1,10 +1,12 @@
 # Main Entry poont python file
 
 from src.utils import label1
+from src.f1 import read_table
 
 
 def main():
-    label1("Panty")
+    label1("Reading Tables")
+    read_table()
 
 
 if __name__ == "__main__":
--- WO/ps1/src/f1.py
+++ WO/ps1/src/f1.py
@@ -1,8 +1,16 @@
-# Main python file for interacting with superpanty 
+# Main python file for interacting with superpanty
 
 import os
 from supabase import create_client, Client
+from rich import print as rprint  # For rprinting
+from rich.pretty import pprint  # For pretty printing
 
+
+# Env setup
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+
+def read_table():
+    response = supabase.table("countries").select("*").execute()
+    pprint(response)
