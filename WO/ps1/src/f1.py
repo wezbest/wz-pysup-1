e86f2e19c0a1031acad0189a07e9c0758b3d356b
wezbest <110793713+wezbest@users.noreply.github.com>
2025-01-05 16:39:06 +0000

    🫦

---
+++ WO/ps1/src/f1.py
@@ -0,0 +1,8 @@
+# Main python file for interacting with superpanty 
+
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
